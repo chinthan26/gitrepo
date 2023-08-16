4712235944950cbaa3c389283382b6d7d0afcc6f
charancs87 <charancs5660@gmail.com>
2023-08-16 12:17:33 +0530

    multiply function is added to arith.py

--- python_progs/arith.py
+++ python_progs/arith.py
@@ -2,10 +2,16 @@ def add(n1,n2):
     return(n1+n2)
 def sub(n1,n2):
     return(n1-n2)
+def mul(n1,n2):
+    return(n1*n2)
+
 a=int(input("Emter a num1 is:"))
 b=int(input("Enter a num2 is:"))
 print("sum=",add(a,b))
 c=int(input("Emter a num1 is:"))
 d=int(input("Enter a num2 is:"))
 print("sum=",sub(c,d))
+e=int(input("Emter a num1 is:"))
+f=int(input("Enter a num2 is:"))
+print("sum=",mul(e,f))
 
